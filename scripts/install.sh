83f1b6b8e6eb009a249a73f115c1d0b926b4ffbc
Matt Montgomery <33811686+ConfusedPolarBear@users.noreply.github.com>
2020-07-09 00:45:29 -0500

    Improve installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,22 +4,37 @@
 set -eu
 
 log() {
-	echo "$(date) $@"
+	echo "$(date) $*"
 }
 
-log Installing Lifeguard
+if [[ $# -ne 2 ]]; then
+	log "Usage: $0 lifeguardUser poolName"
+fi
+
+user="$1"		# User that lifeguard will run as
+pool="$2"		# Pool name to permit lifeguard to access
+
+log "Installing Lifeguard"
 
 if [[ "$EUID" -ne "0" ]]; then
-	log This script must be run as root.
+	log "Error: this script must be run as root"
 	exit 1
 fi
 
 # =========== sudo policy ===========
-log Installing new sudo policy as 99-lifeguard
+log "Installing new sudo policy as 99-lifeguard"
 cp assets/99-lifeguard /etc/sudoers.d/
 
+# =========== zpool permissions ===========
+log "Allowing user $user to perform the following actions on pool $pool:"
+log "    View the differences between a dataset and it's snapshot (diff)"
+log "    Load and unload encryption keys (load-key)"
+log "    Mount and unmount datasets (mount)"
+log "    Create snapshots of datasets (snapshot)"
+zfs allow -d -u "$user" diff,load-key,mount,snapshot "$pool"
+
 # =========== browser permissions ===========
-log Setting browser permissions and ownership
+log "Setting browser permissions and ownership"
 # The browser binary needs to be SUID root and r-xr-xr-x
 chown root browser
 chmod 4555 browser
@@ -29,4 +44,4 @@ chown root config/browser.ini
 chmod 0600 config/browser.ini
 
 # =========== done ===========
-log Installation successful
+log "Installation successful"
